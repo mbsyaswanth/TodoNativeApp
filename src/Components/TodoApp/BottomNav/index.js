d507c7759ff990172da3ae692c0d4e30fd247d2d
M B S Yaswanth <mbs.yaswanth@gmail.com>
2019-08-10 12:14:57 +0530

    added header component

--- src/Components/TodoApp/BottomNav/index.js
+++ src/Components/TodoApp/BottomNav/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BottomNavigation, ActionButton } from "react-native-material-ui";
+import { BottomNavigation } from "react-native-material-ui";
 import { View, Text, StyleSheet, Dimensions } from "react-native";
 class BottomNav extends Component {
   state = {
@@ -7,19 +7,11 @@ class BottomNav extends Component {
   };
 
   styles = StyleSheet.create({
-    bigBlue: {
-      color: "blue",
-      fontWeight: "bold",
-      fontSize: 30
-    },
     container: {
       flex: 1
     },
-    red: {
-      color: "red"
-    },
     add: {
-      bottom: 60
+      bottom: 70
     },
     bottomNav: {
       position: "absolute",
@@ -31,31 +23,27 @@ class BottomNav extends Component {
 
   render() {
     return (
-      <View style={this.styles.container}>
-        <ActionButton style={{ positionContainer: this.styles.add }} />
-
-        <View style={this.styles.bottomNav}>
-          <BottomNavigation active={this.state.active} hidden={false}>
-            <BottomNavigation.Action
-              key="all"
-              icon="list"
-              label="all"
-              onPress={() => this.setState({ active: "all" })}
-            />
-            <BottomNavigation.Action
-              key="active"
-              icon="lock-open"
-              label="Active"
-              onPress={() => this.setState({ active: "active" })}
-            />
-            <BottomNavigation.Action
-              key="completed"
-              icon="check-circle"
-              label="Completed"
-              onPress={() => this.setState({ active: "completed" })}
-            />
-          </BottomNavigation>
-        </View>
+      <View style={this.styles.bottomNav}>
+        <BottomNavigation active={this.state.active} hidden={false}>
+          <BottomNavigation.Action
+            key="all"
+            icon="list"
+            label="all"
+            onPress={() => this.setState({ active: "all" })}
+          />
+          <BottomNavigation.Action
+            key="active"
+            icon="lock-open"
+            label="Active"
+            onPress={() => this.setState({ active: "active" })}
+          />
+          <BottomNavigation.Action
+            key="completed"
+            icon="check-circle"
+            label="Completed"
+            onPress={() => this.setState({ active: "completed" })}
+          />
+        </BottomNavigation>
       </View>
     );
   }
--- src/Components/TodoApp/index.js
+++ src/Components/TodoApp/index.js
@@ -1,9 +1,39 @@
 import React, { Component } from "react";
 import BottomNav from "./BottomNav";
+import { ActionButton } from "react-native-material-ui";
+import { View, StyleSheet, Text, SafeAreaView, StatusBar } from "react-native";
 
 class TodoApp extends Component {
+  styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+    add: {
+      bottom: 60
+    },
+    headerText: {
+      textAlign: "center",
+      color: "white"
+    },
+    header: {
+      backgroundColor: "blue",
+      height: 40,
+      color: "white",
+      justifyContent: "center",
+      marginTop: StatusBar.currentHeight
+    }
+  });
   render() {
-    return <BottomNav />;
+    return (
+      <View style={this.styles.container}>
+        <View style={this.styles.header}>
+          <Text style={this.styles.headerText}>Todo App</Text>
+        </View>
+
+        <ActionButton style={{ positionContainer: this.styles.add }} />
+        <BottomNav />
+      </View>
+    );
   }
 }
 
